26b69ac9f7248c0dff448d971cd8ba7c00b613c6
Jaimelah <JLH520@alumnos.unican.es>
2020-02-20 13:03:14 +0100

    1.0 implementacion de clase State y de esatdos de la practica 3

---
+++ practica3/src/main/java/practica3/AlarmaHogar.java
@@ -0,0 +1,30 @@
+package practica3;
+
+public class AlarmaHogar {
+	private AlarmaHogarState state;
+	private int intervaloSalida;
+	private int intervaloDesactivacion;
+	private String codigoDesactivacion;
+	public void notificarCentralita() {
+		
+	}
+	public void desactivarSensores() {
+		
+	}
+	public void activarSensores() {
+		
+	}
+	public void alarmaOff(String codigo) {
+		
+	}
+	public void alarmaOn() {
+		
+	}
+	public void intruso() {
+		
+	}
+	public void off() {
+		
+	}
+
+}
---
+++ practica3/src/main/java/practica3/AlarmaHogarState.java
@@ -0,0 +1,36 @@
+package practica3;
+
+public abstract class AlarmaHogarState {
+	
+	private static CentralitaNotificada estadoCentralitaNotificada=new CentralitaNotificada();
+	private static Apagada estadoApagada=new Apagada();
+	private static SensoresActivados estadoSensoresActivados=new SensoresActivados();
+	private static IntrusoDetectado estadoIntrusoDetectado=new IntrusoDetectado();
+	private static EsperandoSalida estadoEsperandoSalida=new EsperandoSalida();
+	
+	public static AlarmaHogarState init(AlarmaHogar context) {
+		
+	}
+
+	public static CentralitaNotificada getEstadoCentralitaNotificada() {
+		return estadoCentralitaNotificada;
+	}
+
+	public static Apagada getEstadoApagada() {
+		return estadoApagada;
+	}
+
+	public static SensoresActivados getEstadoSensoresActivados() {
+		return estadoSensoresActivados;
+	}
+
+	public static EsperandoSalida getEstadoEsperandoSalida() {
+		return estadoEsperandoSalida;
+	}
+
+	public static IntrusoDetectado getEstadoIntrusoDetectado() {
+		return estadoIntrusoDetectado;
+	}
+	
+
+}
